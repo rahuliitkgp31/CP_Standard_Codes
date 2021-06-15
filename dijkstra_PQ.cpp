bbb5a1a6c3ec867733ca78b086a1a6569f0388cd
Rahul saxena <40542086+rahuliitkgp31@users.noreply.github.com>
2021-06-15 16:46:31 +0530

    Update dijkstra_PQ.cpp

--- dijkstra_PQ.cpp
+++ dijkstra_PQ.cpp
@@ -1,36 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
 
-class graph {
+class Graph{
 public:
-    vector<pair<int, int> >* adjList;
-    vector<int> dis;
-    int n;
-    vector<int> visited;
-    graph(int x) {
-        n = x;
-        adjList = new vector<pair<int, int> >[x];
-        dis.resize(x);
-        visited.resize(x);
+    vector<pair<ll, ll> >* adjList;
+    ll n;
+    Graph(ll n){
+        this->n = n;
+        adjList = new vector<pair<ll, ll> >[n];
     }
-    void addEdge(int u, int v, int w) {
-        adjList[u].push_back({v, w});
-        adjList[v].push_back({u, w});
+    void addEdge(ll a, ll b, ll w){
+        adjList[a].push_back({b, w});
     }
-    void dijkstra(int src) {
-        for (auto& ele : dis) ele = INT_MAX;
-        priority_queue<pair<int, int>, vector<pair<int, int> >, greater<pair<int, int> > >  pq;
+    ll dijkstra(ll src, ll des){
+        vector<ll> dis(n, INT_MAX);
+        vector<ll> vis(n);
+        priority_queue<pair<ll, ll>, vector<pair<ll, ll>>, greater<pair<ll, ll> > > pq;
         pq.push({0, src});
-        dis[src] = 0;
-        while (!pq.empty()) {
-            pair<int, int> node = pq.top();
+        dis[src]=0;
+        while(!pq.empty()){
+            ll node = pq.top().second;
             pq.pop();
-            if (visited[node.second]) continue;
-            visited[node.second] = 1;
-            for (auto ch : adjList[node.second]) {
-                if (dis[ch.first] > dis[node.second] + ch.second) {
-                    dis[ch.first] = dis[node.second] + ch.second;
-                    pq.push({dis[ch.first], ch.first});
+            if(vis[node]) continue;
+            vis[node]=1;
+            for(ll i=0; i<adjList[node].size(); i++){
+                ll ch = adjList[node][i].first;
+                ll edgeWei = adjList[node][i].second;
+                if(dis[ch]>dis[node]+edgeWei){
+                    dis[ch]=dis[node]+edgeWei;
+                    pq.push({dis[ch], ch});
                 }
             }
         }
+        return dis[des];
     }
 };
+
+int main(){
+    ll t;
+    cin>>t;
+    while(t--){
+        ll n, m;
+        cin>>n>>m;
+        Graph g(n);
+        for(ll i=0; i<m; i++){
+            ll a, b, c;
+            cin>>a>>b>>c;
+            --a, --b;
+            g.addEdge(a, b, c);
+        }
+        ll src, des;
+        cin>>src>>des;
+        --src, --des;
+        ll ans = g.dijkstra(src, des);
+        if(ans==INT_MAX){
+            cout<<"NO"<<endl;
+        }
+        else{
+            cout<<ans<<endl;
+        }
+    }
+    return 0;
+}
