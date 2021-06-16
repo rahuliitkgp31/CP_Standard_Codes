0e0f0470622b3df9ac96d4b3e21df0faaacb4e14
Rahul saxena <40542086+rahuliitkgp31@users.noreply.github.com>
2021-06-16 19:11:46 +0530

    Update kadane.cpp

--- kadane.cpp
+++ kadane.cpp
@@ -2,24 +2,34 @@
 using namespace std;
 typedef long long int ll;
 
-ll kadane(vector<ll>& v) {
+ll kadane(vector<ll>& v){
     ll ans = INT_MIN;
     ll currSum = 0;
-    for (ll i = 0; i < v.size(); i++) {
-        currSum += v[i];
-        ans = max(ans, currSum);
-        if (currSum < 0) {
-            currSum = 0;
+    ll l = 0;
+    ll r = 0;
+    pair<ll, ll> pos;
+    for(ll i=0; i<v.size(); i++){
+        currSum+=v[i];
+        if(currSum>ans){
+            ans = currSum;
+            r=i;
+            pos.first=l;
+            pos.second=r;
+        }
+        if(currSum<0){
+            l=i+1;
+            currSum=0;
         }
     }
+    cout<<pos.first<<" "<<pos.second<<endl;
     return ans;
 }
 
-int main() {
+int main(){
     ll n;
-    cin >> n;
+    cin>>n;
     vector<ll> v(n);
-    for (auto& ele : v) cin >> ele;
-    cout << kadane(v) << endl;
+    for(auto& ele: v) cin>>ele;
+    cout<<kadane(v)<<endl;
     return 0;
 }
